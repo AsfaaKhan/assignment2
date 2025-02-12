a9d939cd3e3c4ec7a2953ac8e1c0b7b2aa22beb6
AsfaaKhan <khanzadiasfi0314@gmail.com>
2025-02-12 20:27:54 +0500

    first commit

--- src/app/Components/Footer/Footer.tsx
+++ src/app/Components/Footer/Footer.tsx
@@ -2,11 +2,14 @@ import cssStyle from "./Footer.module.css"
 const Footer = () => {
     return(
         <div>
-            <ul className={cssStyle.footer}>
+            <div className={cssStyle.footer}>
+            <ul  className="grid grid-cols-2 sm:grid-cols-3" >
                 <li className={cssStyle.item}><a href="https://github.com/AsfaaKhan" target="_blank" >GitHub</a></li>
                 <li className={cssStyle.item}><a href="https://www.facebook.com/" target="_blank">FaceBook</a></li>
                 <li className={cssStyle.item}><a href="https://www.instagram.com/" target="_blank">Instagram</a></li>
             </ul>
+            </div>
+            
         </div>
     )
 
--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -4,17 +4,13 @@ const Header = () => {
     return (
         <div>
             <nav className={cssStyle.navbar}>
-
-                <ul>
+                <ul className="gird grid-cols-2 sm:grid-cols-3">
                     <li className={cssStyle.nav_item}><Link href="/home">Home</Link></li>
                     <li className={cssStyle.nav_item}><Link href="/about">About</Link></li>
                     <li className={cssStyle.nav_item}><Link href="/contact">Contact</Link></li>
-
                 </ul>
             </nav>
-
         </div>
-
     )
 }
 export default Header
